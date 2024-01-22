71439126ba5bc7a398d063321edd8dd5683c3e4c
Lucas Dias <lucas.allx@hotmail.com>
2024-01-22 19:18:40 -0300

    banner initial

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,10 @@ import BannerComponent from './components/BannerComponent.vue';
 </script>
 
 <style>
+*{
+  margin:0px;
+  padding:0px;
+  box-sizing: border-box;
+}
 
 </style>
--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,10 +1,38 @@
 <template>
     <div>
-        banner
+      <q-carousel
+        swipeable
+        animated
+        v-model="slide"
+        thumbnails
+        infinite
+        :autoplay="autoplay"
+        @mouseenter="autoplay = false"
+        @mouseleave="autoplay = true"
+      >
+
+        <q-carousel-slide :name="1" img-src="../assets/banners/michael-jordan.jpg">
+            <div class="text-container">
+                <h1>Slide 1</h1>
+                <h2>Texto descritivo para o Slide 1.</h2>
+            </div>
+        </q-carousel-slide>
+
+        <q-carousel-slide :name="2" img-src="../assets/banners/lebron-james.jpg" />
+        
+        <q-carousel-slide :name="3" img-src="../assets/banners/kobe-bryant.jpg" />
+
+      </q-carousel>
+      
     </div>
-</template>
+  </template>
 
 <script setup>
+import { ref } from 'vue'
+
+  const slide = ref(1)
+  const autoplay = ref(true)
+    
 
 </script>
 
