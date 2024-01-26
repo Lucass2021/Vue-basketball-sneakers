17a9703de1ae6a5286d6a309cac507cd957f21d9
Lucas Dias <lucas.allx@hotmail.com>
2024-01-26 19:08:47 -0300

    jordan finished

--- src/components/SneakerComponent.vue
+++ src/components/SneakerComponent.vue
@@ -3,6 +3,11 @@
         <div class="img-container">
             <img src="../assets/sneakers/jordan/jordan-green.png" alt="">
         </div>
+        <div class="text-container">
+            <h4>Air Jordan 1 Mid Dutch Green</h4>
+            <h5>Tênis Air Jordan</h5>
+            <p>R$ 1199,99</p>
+        </div>
     </div>
 </template>
 
@@ -16,14 +21,37 @@
         width: 100%;
         display: flex;
         justify-content: center;
+        align-items: center;
+        flex-direction: column;
     }
-
-    .img-container{
-        background: #F3F7FF;
-    }
-
     .img-container img{
-        width: 200px;
+        width: 300px;
+        background: #F3F7FF;
+        padding:30px;
+    }
+
+    .text-container{
+        width: 300px;
+    }
+
+    .text-container h4{
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 18px;
+        line-height: 20px;
+        margin-top: 20px;
+        margin-bottom: 5px;
+    }
+
+    .text-container h5{
+        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+        font-size: 18px;
+        margin-bottom: 10px;
+        color: #7CA2F4;
+    }
+
+    .text-container p{
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 20px;
     }
 
 </style>
